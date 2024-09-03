f67ce2e9c0b9ecc9d419c4277d10b296f76ac81d
unknown <hammadurrehman7707@gmail.com>
2024-09-03 22:11:20 +0500

    Create maximum.cpp

---
+++ Lecture-1/maximum.cpp
@@ -0,0 +1,28 @@
+// a program to check maximum number.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    // take values in input
+    int a, b;
+    cout << "Enter first number:\n ";
+    cin >> a;
+    cout << "\nEnter second number:\n ";
+    cin >> b;
+    // check maximum number
+    if (a > b)
+    {
+        cout << "\nMaximum number is " << a;
+    }
+    else if (b > a)
+    {
+        cout << "\nMaximum number is " << b;
+    }
+    else
+    {
+        cout << "\nBoth numbers are equal";
+    }
+    getch();
+    return 0;
+}
